5bff3138ebe1b273289699e24b7d4aca5b1ba77e
Yuri Gorokhov <yurigorokhov@gmail.com>
2014-11-14 18:31:36 -0800

    Initial Commit

---
+++ DataStructures/DataStructures.h
@@ -0,0 +1,19 @@
+//
+//  DataStructures.h
+//  DataStructures
+//
+//  Created by Yuri Gorokhov on 11/14/14.
+//  Copyright (c) 2014 Yuri Gorokhov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DataStructures.
+FOUNDATION_EXPORT double DataStructuresVersionNumber;
+
+//! Project version string for DataStructures.
+FOUNDATION_EXPORT const unsigned char DataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStructures/PublicHeader.h>
+
+
